2cba9bae82a19cfb4590c2898341b9467de4bf20
Prasad Busam <121351552+prasadbusam@users.noreply.github.com>
2025-11-27 14:44:34 +0530

    Update class.js
    yes

--- class.js
+++ class.js
@@ -1,6 +1,6 @@
 class prasad{
     m1(){
-        console.log("This is prasad m1 method")
+        console.log("This is prasad m1 method today")
     }
     m2(){
         console.log("This is m2 method")
